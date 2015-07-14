9724cd7ad7456713dce9536b9675a0bdbaeb9b74
Thomas Hernandez <thomasah12@ymail.com>
2015-07-14 12:01:52 -0500

    added imdb_ids to proccesing

--- writing to db.py
+++ writing to db.py
@@ -1,11 +1,13 @@
 import os
 import dataset
 import time
-# db = dataset.connect('sqlite:///C:\\Users\\Tom\\Documents\\GitHub\\un-watched_status\\test files\\video_list.db')
+from imdbpie import Imdb
+# db = dataset.connect('sqlite:///C:\\Users\Tom\\PycharmProjects\\un-watched_status\\video_list.db')
 db = dataset.connect('sqlite:///C:\\Users\Tom\\video_list.db')
+imdb = Imdb()
 
 
-def folder_crawler(path_to_craw, unwatched, drop):
+def folder_crawler(path_to_craw, unwatched, drop, add_id):
     start_time = time.time()
     db_name = path_to_craw[3:]
     print(db_name + " Starting")
@@ -17,16 +19,24 @@ def folder_crawler(path_to_craw, unwatched, drop):
             file_path = os.path.join(dir_path, file_name)
             file_size = os.path.getsize(file_path)
             file_size_bytes = file_size/1073741824
-            without_mp4 = file_name[:-3]
+            without_mp4 = file_name[:-4]
             if status:
                 view_status = "Unwatched"
             else:
                 view_status = ""
 
-            if table.find_one(filename=file_name) is None:
-                table.insert(dict(filename=file_name, path=dir_path,
-                                  file_size=(str(round(file_size_bytes, 2)) + ' GB'), status=view_status,
-                                  movie_name=without_mp4))
+            if add_id:
+                movie = imdb.search_for_title(without_mp4)
+                movie_id = movie[0]['imdb_id']
+                if table.find_one(filename=file_name) is None:
+                    table.insert(dict(filename=file_name, path=dir_path,
+                                      file_size=(str(round(file_size_bytes, 2)) + ' GB'), status=view_status,
+                                      movie_name=without_mp4, imdb_id=movie_id))
+            else:
+                if table.find_one(filename=file_name) is None:
+                    table.insert(dict(filename=file_name, path=dir_path,
+                                      file_size=(str(round(file_size_bytes, 2)) + ' GB'), status=view_status,
+                                      movie_name=without_mp4))
 
     def start_file_op():
             folder = path_to_craw
@@ -39,6 +49,5 @@ def folder_crawler(path_to_craw, unwatched, drop):
     start_file_op()
     print(db_name + " Was Done In " + str(round((time.time() - start_time), 2)) + " seconds")
 
-folder_crawler(r"E:\Movies", True, False)
-folder_crawler(r"E:\TV", False, False)
-input(' ')
+folder_crawler(r"E:\Movies", True, False, True)
+folder_crawler(r"E:\TV", False, False, False)
