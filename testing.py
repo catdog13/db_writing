d81f4bf58b0a195df877c6f4bdbf13a5e35cd814
Thomas Hernandez <thomasah12@ymail.com>
2015-07-15 22:04:47 -0500

    Testing

--- testing.py
+++ testing.py
@@ -1,9 +1,10 @@
 import dataset
+from modules import *
 db = dataset.connect('sqlite:///C:\\Users\Tom\\PycharmProjects\\db_writing\\db_files\\video_list.db')
 db_name = 'Movies'
 table = db[db_name]
 
 for x in range(1, 2):
-    movie_name = table.find_one(id=x)['movie_name']
-    data = dict(id=x, movie_name=movie_name)
+    movie_path = table.find_one(id=x)['path']
+    data = dict(id=x, file_size=get_movie_size(movie_path))
     table.update(data, ['id'])
