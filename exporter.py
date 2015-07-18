741c453817aa910b1038a458c86ea14a6a23afa8
Thomas Hernandez <thomasah12@ymail.com>
2015-07-18 15:20:57 -0500

    Added a Stand alone expoerter for the db

---
+++ exporter.py
@@ -0,0 +1,7 @@
+import dataset
+db = dataset.connect('sqlite:///C:\\Users\Tom\\video_list.db')
+table = db['Movies']
+
+dataset.freeze(table, format='csv', filename='db_files\\Movie_list.csv')
+dataset.freeze(table, format='json', filename='db_files\\Movie_list.json')
+dataset.freeze(table, format='tabson', filename='db_files\\Movie_list.tabson')
