ae899a761bd2a51b00ab4a5a4a0e4ab33f6a7331
Thomas Hernandez <thomasah12@ymail.com>
2015-08-08 23:07:33 -0500

    Path Changes

--- updater.py
+++ updater.py
@@ -1,9 +1,9 @@
 import dataset
 from modules import *
 
-db = dataset.connect('sqlite:///db_files\\video_list.db')
+db = dataset.connect('sqlite:///C:\\Users\\Tom\Documents\\Python_Projects\\db_writing\\db_files\\video_list.db')
 db_name = 'Movies'
 table = db[db_name]
 
-for x in range(1, 2):
+for x in range(1, 384):
     update(table, x)
