0b88893756da2368739f6d305eecc40ca67ab26b
Thomas Hernandez <thomasah12@ymail.com>
2015-09-21 23:23:12 -0500

    Now using TV

--- modules.py
+++ modules.py
@@ -123,70 +123,32 @@ class ForMovies:
 
 
 class ForTv:
-    def __init__(self, series_name, show_path, table, file_name):
+    def __init__(self, series_name, table):
         self.series_name = series_name.split(' - ')[0]
-        self.show_path = show_path
         self.table = table
-        self.file_name = file_name
     json_file = {}
     status = True
 
-    def get_view_status(self):
-        if self.status:
-            view_status = 'Unwatched'
-        else:
-            view_status = 'Watched'
-        return view_status
-
-    def get_show_size(self):
-        file_size = os.path.getsize(self.show_path)
-        file_size_bytes = file_size / 1073741824
-        return str(round(file_size_bytes, 2)) + ' GB'
-
-    def get_season(self):
-        season_episode = self.file_name.split(' - ')[1]
-        season = season_episode[1:-3]
-        return season
-
-    def get_episode(self):
-        season_episode = self.file_name.split(' - ')[1]
-        episode = season_episode[4:]
-        return episode
-
     def get_json(self):
         show_name = self.series_name.replace('&', '%26')
-        json_file = requests.get('http://www.omdbapi.com/?t=' + show_name +
-                                 '&Season=' + self.get_season() + '&Episode=' + self.get_episode())
+        show_name = show_name.split('(')[0]
+        if show_name == 'Agents of S.H.I.E.L.D':
+            show_name = 'Agents of S.H.I.E.L.D.'
+        json_file = requests.get('http://www.omdbapi.com/?t=' + show_name + '&type=series')
         self.json_file = json_file.json()
         return json_file.json()
+
+    def get_year(self):
+        year = self.json_file['Year']
+        return year
 
     def get_runtime(self):
         runtime = self.json_file['Runtime']
         return runtime
 
-    def get_series_id(self):
-        series_id = self.json_file['seriesID']
-        return series_id
-
-    def get_episode_id(self):
-        episode_id = self.json_file['imdbID']
-        return episode_id
-
-    def get_show_title(self):
-        show_title = self.json_file['Title']
-        return show_title
-
     def get_show_genre(self):
         show_genre = self.json_file['Genre']
         return show_genre
-
-    def get_episode_release_date(self):
-        episode_release = self.json_file['Released']
-        release_year = episode_release[-4:]
-        release_month = episode_release[3:][:4]
-        release_day = episode_release[:2]
-        episode_release_date = release_year + ' ' + release_month + ' ' + release_day
-        return episode_release_date
 
     def get_show_plot(self):
         show_plot = self.json_file['Plot']
@@ -196,58 +158,29 @@ class ForTv:
         actors = self.json_file['Actors']
         return actors
 
+    def get_imdb_rating(self):
+        rating = self.json_file['imdbRating']
+        return rating
+
+    def get_imdb_id(self):
+        show_id = self.json_file['imdbID']
+        return show_id
+
     def get_length(self):
         length = len(self.table) + 1
         return length
 
-    def update(self, x):
+    def writer(self):
         series_name = self.series_name
         self.get_json()
-        data = dict(id=x,
-                    imdb_series_id=self.get_series_id(),
-                    imdb_episode_id=self.get_episode_id(),
-                    title=self.get_show_title(),
-                    file_size=self.get_show_size(),
+        data = dict(id=self.get_length(),
+                    release_year=self.get_year(),
                     runtime=self.get_runtime(),
                     genre=self.get_show_genre(),
-                    release_date=self.get_episode_release_date(),
                     plot=self.get_show_plot(),
-                    show_name=series_name,
-                    path=self.show_path,
                     actors=self.get_actors(),
-                    episode=self.get_episode(),
-                    season=self.get_season(),
+                    rating=self.get_imdb_rating(),
+                    imDB_ID=self.get_imdb_id(),
+                    show_name=series_name,
                     )
-        self.table.update(data, ['id'])
-
-    def writer(self):
-        if self.table.find_one(path=self.show_path) is None:
-            series_name = self.series_name
-            self.get_json()
-            data = dict(id=self.get_length(),
-                        imdb_series_id=self.get_series_id(),
-                        imdb_episode_id=self.get_episode_id(),
-                        title=self.get_show_title(),
-                        file_size=self.get_show_size(),
-                        runtime=self.get_runtime(),
-                        genre=self.get_show_genre(),
-                        release_date=self.get_episode_release_date(),
-                        plot=self.get_show_plot(),
-                        show_name=series_name,
-                        path=self.show_path,
-                        actors=self.get_actors(),
-                        episode=self.get_episode(),
-                        season=self.get_season(),
-                        )
-            self.table.insert(data)
-
-    def writer_simple(self):
-        if self.table.find_one(path=self.show_path) is None:
-            show_name = self.file_name
-            self.get_json()
-            data = dict(id=self.get_length(),
-                        file_size=self.get_show_size(),
-                        movie_name=show_name,
-                        path=self.show_path,
-                        )
-            self.table.insert(data)
+        self.table.insert(data)
--- writing_to_db.py
+++ writing_to_db.py
@@ -9,14 +9,20 @@ def folder_crawler(path_to_craw):
     start_time = time.time()
     db_name = path_to_craw[3:]
     print(db_name + ' Starting')
-    table = db[db_name]
 
     def file_walker():
-        for root, subdir, files in os.walk(path_to_craw):
-            for file in files:
-                if file.endswith('.mp4'):
-                    full_path = os.path.join(root, file)
-                    modules.ForMovies(file[:-4], full_path, table).writer()
+        if path_to_craw.endswith('Movies'):
+            table = db[db_name]
+            for root, subdir, files in os.walk(path_to_craw):
+                for file in files:
+                    if file.endswith('.mp4'):
+                        full_path = os.path.join(root, file)
+                        modules.ForMovies(file[:-4], full_path, table).writer()
+        elif path_to_craw.endswith('TV'):
+            db[db_name].drop()
+            table = db[db_name]
+            for shows in os.scandir(path_to_craw):
+                modules.ForTv(shows.name, table).writer()
 
     file_walker()
     print(db_name + ' Was Done In ' + str(round((time.time() - start_time), 2)) + ' seconds')
@@ -24,3 +30,4 @@ def folder_crawler(path_to_craw):
 
 if __name__ == '__main__':
     folder_crawler(r'D:\Movies')
+    folder_crawler(r'D:\TV')
