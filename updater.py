1ba7bfd63a178782ec236e4936d8d3fc4670d8d7
Thomas Hernandez <thomasah12@ymail.com>
2016-05-13 22:32:05 -0500

    Made some changes to the updater

--- updater.py
+++ updater.py
@@ -1,13 +1,17 @@
 import dataset
 import modules
+from time import sleep
 
 db = dataset.connect('sqlite:///C:\\Users\\Tom\Documents\\Python_Projects\\db_writing\\db_files\\video_list.db')
 db_name = 'Movies'
 table = db[db_name]
-lower = 1
-upper = len(table) + 501
+lower = 202
+upper = len(table) + 45
 
 for x in range(lower, upper):
+    print('id is ' + str(x))
     movie_path = table.find_one(id=x)['path']
     movie_name = table.find_one(id=x)['movie_name']
     modules.ForMovies(movie_name, movie_path, table).update(x)
+    print(movie_name + ' is done')
+    sleep(1)
