28faaefe7eaf97eba7d1ce3c48f16b1688a1363f
Thomas Hernandez <thomasah12@ymail.com>
2017-10-05 23:05:40 -0500

    Clean Up

--- file_time.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import os
-import time
-
-file_info = os.stat('/media/tom/arrrrrrrrr/Movies/Terminator Genisys (2015)/Terminator Genisys.mp4')
-# print(file_info.st_ctime)
-ucttime = time.gmtime(file_info.st_ctime)
-localtime = time.localtime(file_info.st_ctime)
-print(localtime)
